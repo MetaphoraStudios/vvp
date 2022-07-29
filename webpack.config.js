844c760a4396773529a95448f312b46962805330
Diogo Neves <diogo.neves@gmail.com>
2022-07-29 14:55:25 +0100

    Webpack fully builds

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const appDirectory = fs.realpathSync(process.cwd());
 module.exports = {
     entry: path.resolve(appDirectory, "src/app.ts"), //path to the main .ts file
     output: {
-        filename: "js/bundleName.js", //name for the js file that is created/compiled in memory
+        filename: "js/appBundle.js",
         clean: true,
     },
     resolve: {
