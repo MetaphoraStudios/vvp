c0da2058889f565bc53f84e40b5838ab774f6b25
Diogo Neves <diogo.neves@gmail.com>
2022-07-30 08:38:09 +0100

    Fixed warnings on dev tools

--- webpack.config.js
+++ webpack.config.js
@@ -4,37 +4,42 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const appDirectory = fs.realpathSync(process.cwd());
 
 module.exports = {
-    entry: path.resolve(appDirectory, "src/app.ts"), //path to the main .ts file
-    output: {
-        filename: "js/appBundle.js",
-        clean: true,
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-    devServer: {
-        host: "0.0.0.0",
-        port: 8080, //port that we're using for local host (localhost:8080)
-        static: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
-        hot: true,
-        devMiddleware: {
-            publicPath: "/",
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            inject: true,
-            template: path.resolve(appDirectory, "public/index.html"),
-        })
+  entry: path.resolve(appDirectory, "src/app.ts"), //path to the main .ts file
+  output: {
+    filename: "js/appBundle.js",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    host: "0.0.0.0",
+    port: 8080, //port that we're using for local host (localhost:8080)
+    static: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
+    hot: true,
+    devMiddleware: {
+      publicPath: "/",
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
     ],
-    mode: "development",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(appDirectory, "public/index.html"),
+    })
+  ],
+  mode: "development",
 };
