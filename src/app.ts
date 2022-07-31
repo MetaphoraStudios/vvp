46b78d627b4a38d617b190ae2c17ff66d5bc7216
Diogo Neves <diogo.neves@gmail.com>
2022-07-31 12:34:13 +0100

    Got textures loading and larger grid

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,13 @@ import {
   Vector3,
   HemisphericLight,
   MeshBuilder,
-  Texture,
-  StandardMaterial
+  RawTexture
 } from "@babylonjs/core";
+import {
+  Image,
+  AdvancedDynamicTexture,
+  Control
+} from "@babylonjs/gui";
 
 class App {
   constructor() {
@@ -43,25 +47,37 @@ class App {
   private createScene(engine: Engine, canvas: HTMLCanvasElement): Scene {
     let scene = new Scene(engine);
 
-    let camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 4, 60, Vector3.Zero(), scene);
+    let camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 200, Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
     new HemisphericLight("light", new Vector3(1, 1, 0), scene);
 
-    let texture = new Texture("../assets/recordings/20220211202818/depth.10037.png", scene);
-    let material = new StandardMaterial("mat", scene);
-    material.diffuseTexture = texture;
-    material.backFaceCulling = false;
-    let plane = MeshBuilder.CreatePlane("plane", { width: 10, height: 10 }, scene);
-    plane.material = material;
+    let depthMap = new Array(100 * 100).fill(0);
+    let anchor = new Vector3(-75, -75, 0);
+    this.createVoxelGrid(depthMap, anchor, scene);
 
-    this.createVoxelGrid([], Vector3.Zero(), scene);
+    let colourImage = this.createImage("debugColour", "assets/recordings/20220211202818/camera.10037.png")
+    let depthImage = this.createImage("debugDepth", "assets/recordings/20220211202818/depth.10037.png");
+    depthImage.top = "110px";
+    let advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+    advancedTexture.addControl(colourImage);
+    advancedTexture.addControl(depthImage);
 
     return scene;
   }
 
+  private createImage(name: string, path: string): Image {
+    let image = new Image(name, path);
+    image.width = "100px";
+    image.height = "100px";
+    image.stretch = Image.STRETCH_UNIFORM;
+    image.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
+    image.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
+    return image;
+  }
+
   private createVoxelGrid(depthMap: number[], anchor: Vector3, scene: Scene) {
-    let width = 10;
-    let height = 10;
+    let width = 100;
+    let height = 100;
     let spacing = 0.5;
     let size = 1.0;
     let offset = size * 0.5;
