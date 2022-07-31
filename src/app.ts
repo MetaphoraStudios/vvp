3ffff5861d9abd3d860b82e255815c1c8fc10036
Diogo Neves <diogo.neves@gmail.com>
2022-07-31 11:48:18 +0100

    Added a simple grid

--- src/app.ts
+++ src/app.ts
@@ -34,14 +34,29 @@ class App {
   private createScene(engine: Engine, canvas: HTMLCanvasElement): Scene {
     let scene = new Scene(engine);
 
-    let camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 4, 6, Vector3.Zero(), scene);
+    let camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 4, 60, Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
-    let light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-    let ground: Mesh = MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
-    let sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
-    sphere.position.y = 0.5;
+    new HemisphericLight("light", new Vector3(1, 1, 0), scene);
+
+    this.createVoxelGrid(Vector3.Zero(), scene);
 
     return scene;
+  }
+
+  private createVoxelGrid(anchor: Vector3, scene: Scene) {
+    let width = 10;
+    let height = 10;
+    let spacing = 0.5;
+    let size = 1.0;
+    let offset = size * 0.5;
+    for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+        let name = `box${x}-${y}`;
+        let box = MeshBuilder.CreateBox(name, { size }, scene);
+        let position = new Vector3(x * (size + spacing) - offset, y * (size + spacing) - offset, 0);
+        box.position = position.add(anchor);
+      }
+    }
   }
 
   private registerInspectorHandler(scene: Scene) {
